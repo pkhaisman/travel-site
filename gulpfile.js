837ed708819b019252cdb8baa4f640a3dc05f609
EC2 Default User <ec2-user@ip-172-31-30-91.us-east-2.compute.internal>
2018-11-28 16:23:52 +0000

    css file architecture changes

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,9 @@ var gulp            = require("gulp"),
     postcss         = require("gulp-postcss"),
     autoprefixer    = require("autoprefixer"),
     cssvars         = require("postcss-simple-vars"),
-    nested          = require("postcss-nested");
+    nested          = require("postcss-nested"),
+    cssimport       = require("postcss-import"),
+    browserSync     = require("browser-sync").create();
 
 gulp.task("default", function() {
     console.log("Hoorah");
@@ -11,15 +13,19 @@ gulp.task("default", function() {
 
 gulp.task("styles", function() {
     return gulp.src("./app/assets/styles/styles.css")
-        .pipe(postcss([cssvars, nested, autoprefixer]))
+        .pipe(postcss([cssimport, cssvars, nested, autoprefixer]))
         .pipe(gulp.dest("./app/temp/styles"));
 });
 
 gulp.task("watch", function() {
+    browserSync.init({
+        server: {
+            baseDir: "app"
+        } 
+    });
     watch("./app/index.html", function() {
         gulp.start("default");
     });
-    
     watch("./app/assets/styles/**/*.css", function() {
         gulp.start("styles"); 
     });
